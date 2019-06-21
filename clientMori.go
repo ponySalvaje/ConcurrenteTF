8ee29de4e544ca64b8e0549b094566a15440502e
eolortegui <eolo@gmail.com>
2019-06-21 10:00:00 -0500

    Cliente 1 agregado

---
+++ clientMori.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"encoding/json"
+)
+
+
+var nodos  map[string]bool
+
+func Client_1(subdir, serverdir string ){
+
+	host := fmt.Sprintf("%s:8001", serverdir)
+	fmt.Println(host)
+	con, _ := net.Dial("tcp",host)
+
+	defer con.Close()
+	fmt.Fprintln(con,subdir)
+
+	r:= bufio.NewReader(con)
+	msg, _ := r.ReadString('\n')
+
+	fmt.Println(msg)
+
+	rest := make(map[string]bool)
+
+	_ =json.Unmarshal([]byte(msg), &rest )
+	fmt.Println(rest)
+
+	for nodo := range rest{
+
+		nodos[nodo]= true
+	}
+	fmt.Println(nodos)
+}
+
+func main(){
+
+
+	nodos = make(map[string]bool)
+
+	nodos["10.21.61.168"] = true
+
+	Client_1("10.21.61.175", "10.21.61.168")
+
+}
+
+
+
+
+
+
+
+
+
