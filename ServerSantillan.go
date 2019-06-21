d32f5c28e357222ba8aa8a14f0d69389d801332a
Mauricio Huiza Tamashiro <juanitopepe@gmail.com>
2019-06-21 09:16:41 -0500

    Server S2 Santillan

---
+++ ServerSantillan.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net"
+	"time"
+)
+
+const (
+	PROT = "tcp"
+	HOST = "10.21.61.184:8002"
+)
+
+var nodes map[string]bool
+
+func handle(con net.Conn) {
+	defer con.Close()
+	r := bufio.NewReader(con)
+	msg, _ := r.ReadString('\n')
+	fmt.Print("Recibido: ", msg)
+	nodes[msg] = true
+}
+
+func main() {
+	nodes = make(map[string]bool)
+	ln, _ := net.Listen(PROT, HOST)
+	defer ln.Close()
+	rand.Seed(time.Now().UTC().UnixNano())
+	for {
+		con, _ := ln.Accept()
+		go handle(con)
+	}
+
+}
