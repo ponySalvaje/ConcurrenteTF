fab13b035d2da93f50b3d792e6886a4e1f493fe6
juanito perez <junitoperz@gmil.com>
2019-06-21 09:45:50 -0500

    Cliente 2 agregado

---
+++ cli2.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	nodos:= make( map [string ] bool)
+	nodos["10.21.61.184"]=true 
+	cli2("hola mundo",nodos)
+}
+
+ func cli2( dir string, nodos map [string ] bool){
+
+	
+	for	nodo := range nodos { 
+		host:=fmt.Sprintf("%s:8002",nodo)
+		fmt.Println(host)
+		con, _ := net.Dial("tcp", host)
+		fmt.Fprintln(con,dir)
+		con.Close()
+
+	}
+ }
